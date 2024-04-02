110e725be9390c7907073eb2cd2bbb682ce43ac1
J.Xulu <ST10457466@vcconnect.edu.za>
2024-04-02 16:12:32 +0200

    first commit

---
+++ app/src/main/java/com/st10457466/budgettracking/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.st10457466.budgettracking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
